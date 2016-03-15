abe3875afcda536eb315922e5b05defdab498948
qingsongyao <454919140@qq.com>
2016-03-15 11:52:52 +0800

    首页完成
    two

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,20 +64,164 @@
             </div>
         </div>
     </nav>
-    <section class="content-warp">
+    <section class="page">
         <div class="container">
-            <div class="row">
-                <main class="col-md-12 border">
-                    <article class="text-center" style="margin: 15px 0px; background-color: #A6C8FF">
-                        111
-                    </article>
-                </main>
+            <div class="row text-center">
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="{{asset('img/1.jpg')}}" width="360" height="" alt="...">
+                        <div class="caption">
+                            <h3>标题</h3>
+                            <p>我还in是的弗兰克时间浪费三级分类接撒李开复萨芬绿色减肥啦啊放假拉喀什减肥啦啊东京拉开决定大家啊了解到</p>
+                            <hr />
+                            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="{{asset('img/1.jpg')}}" width="360" height="" alt="...">
+                        <div class="caption">
+                            <h3>标题</h3>
+                            <p>我还in是的弗兰克时间浪费三级分类接撒李开复萨芬绿色减肥啦啊放假拉喀什减肥啦啊东京拉开决定大家啊了解到</p>
+                            <hr />
+                            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="{{asset('img/1.jpg')}}" width="360" height="" alt="...">
+                        <div class="caption">
+                            <h3>标题</h3>
+                            <p>我还in是的弗兰克时间浪费三级分类接撒李开复萨芬绿色减肥啦啊放假拉喀什减肥啦啊东京拉开决定大家啊了解到</p>
+                            <hr />
+                            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="{{asset('img/1.jpg')}}" width="360" height="" alt="...">
+                        <div class="caption">
+                            <h3>标题</h3>
+                            <p>我还in是的弗兰克时间浪费三级分类接撒李开复萨芬绿色减肥啦啊放假拉喀什减肥啦啊东京拉开决定大家啊了解到</p>
+                            <hr />
+                            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="{{asset('img/1.jpg')}}" width="360" height="" alt="...">
+                        <div class="caption">
+                            <h3>标题</h3>
+                            <p>我还in是的弗兰克时间浪费三级分类接撒李开复萨芬绿色减肥啦啊放假拉喀什减肥啦啊东京拉开决定大家啊了解到</p>
+                            <hr />
+                            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="{{asset('img/1.jpg')}}" width="360" height="" alt="...">
+                        <div class="caption">
+                            <h3>标题</h3>
+                            <p>我还in是的弗兰克时间浪费三级分类接撒李开复萨芬绿色减肥啦啊放假拉喀什减肥啦啊东京拉开决定大家啊了解到</p>
+                            <hr />
+                            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="{{asset('img/1.jpg')}}" width="360" height="" alt="...">
+                        <div class="caption">
+                            <h3>标题</h3>
+                            <p>我还in是的弗兰克时间浪费三级分类接撒李开复萨芬绿色减肥啦啊放假拉喀什减肥啦啊东京拉开决定大家啊了解到</p>
+                            <hr />
+                            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="{{asset('img/1.jpg')}}" width="360" height="" alt="...">
+                        <div class="caption">
+                            <h3>标题</h3>
+                            <p>我还in是的弗兰克时间浪费三级分类接撒李开复萨芬绿色减肥啦啊放假拉喀什减肥啦啊东京拉开决定大家啊了解到</p>
+                            <hr />
+                            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="{{asset('img/1.jpg')}}" width="360" height="" alt="...">
+                        <div class="caption">
+                            <h3>标题</h3>
+                            <p>我还in是的弗兰克时间浪费三级分类接撒李开复萨芬绿色减肥啦啊放假拉喀什减肥啦啊东京拉开决定大家啊了解到</p>
+                            <hr />
+                            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
-    <footer>
+    <footer class="main-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="widget">
+                        <h4 class="title">友情链接</h4>
+                        <div class="content friend-links">
+                            <a href="http://www.bootcss.com/" title="Bootstrap中文网" onclick="_hmt.push(['_trackEvent', 'link', 'click', 'Bootstrap中文网'])" target="_blank">Bootstrap中文网</a>
+                            <a href="http://www.golaravel.com/" title="Laravel中文网" onclick="_hmt.push(['_trackEvent', 'link', 'click', 'Laravel中文网'])" target="_blank">Laravel中文网</a>
+                            <a href="http://www.golaravel.com/" title="Laravel中文网" onclick="_hmt.push(['_trackEvent', 'link', 'click', 'Laravel中文网'])" target="_blank">Laravel中文网</a>
+                            <a href="http://www.golaravel.com/" title="Laravel中文网" onclick="_hmt.push(['_trackEvent', 'link', 'click', 'Laravel中文网'])" target="_blank">Laravel中文网</a>
+                            <a href="http://www.golaravel.com/" title="Laravel中文网" onclick="_hmt.push(['_trackEvent', 'link', 'click', 'Laravel中文网'])" target="_blank">Laravel中文网</a>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="col-sm-4">
+                    <div class="widget">
+                        <h4 class="title">我用到的技术</h4>
+                        <div class="content tag-cloud">
+                            <a class="btn btn-default" href="http://www.bootcss.com/" title="Bootstrap中文网" onclick="_hmt.push(['_trackEvent', 'link', 'click', 'Bootstrap中文网'])" target="_blank">Bootstrap</a>
+                            <a class="btn btn-info" href="http://www.ghostchina.com/" title="Ghost中国" onclick="_hmt.push(['_trackEvent', 'link', 'click', 'Ghost中国'])" target="_blank">Ghost</a>
+                            <a class="btn btn-warning" href="http://www.bootcdn.cn/" title="BootCDN" onclick="_hmt.push(['_trackEvent', 'link', 'click', 'BootCDN'])" target="_blank">BootCDN</a>
+                            <a class="btn btn-danger" href="http://www.gruntjs.net/" title="Grunt中文网" onclick="_hmt.push(['_trackEvent', 'link', 'click', 'Grunt中文网'])" target="_blank">Grunt</a>
+                            <a class="btn btn-success" href="http://www.jquery123.com/" title="jQuery中文文档" onclick="_hmt.push(['_trackEvent', 'link', 'click', 'jQuery中文文档'])" target="_blank">jQuery</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="widget">
+                        <h4 class="title">赞助商</h4>
+                        <div class="content friend-links">
+                            <a href="https://www.upyun.com/" title="又拍云" onclick="_hmt.push(['_trackEvent', 'link', 'click', '又拍云'])" target="_blank">又拍云</a>
+                            <a href="https://www.upyun.com/" title="又拍云" onclick="_hmt.push(['_trackEvent', 'link', 'click', '又拍云'])" target="_blank">又拍云</a>
+                            <a href="https://www.upyun.com/" title="又拍云" onclick="_hmt.push(['_trackEvent', 'link', 'click', '又拍云'])" target="_blank">又拍云</a>
+                            <a href="https://www.upyun.com/" title="又拍云" onclick="_hmt.push(['_trackEvent', 'link', 'click', '又拍云'])" target="_blank">又拍云</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </footer>
+    <div class="copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <span>Copyright © <a href="http://isfp.cn">道长博客</a></span> |
+                    <span><a href="http://www.miibeian.gov.cn/" target="_blank" rel="">京ICP备15041807号</a></span>
+                </div>
+            </div>
+        </div>
+    </div>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
